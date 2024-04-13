9fdeac8259119e9224bc8219b26b1fa743a4fedd
Subham <bhandarimaiya65@gmail.com>
2024-04-13 21:28:07 +0545

    home page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+import Home from "./components/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [openSidebarToggle, setOpenSidebarToggle] = useState(false);
 
-  return <></>;
+  const OpenSidebar = () => {
+    setOpenSidebarToggle(!openSidebarToggle);
+  };
+
+  return (
+    <div className="grid-container">
+      <Header OpenSidebar={OpenSidebar} />
+      <Sidebar
+        openSidebarToggle={openSidebarToggle}
+        OpenSidebar={OpenSidebar}
+      />
+      <Home />
+    </div>
+  );
 }
 
 export default App;
